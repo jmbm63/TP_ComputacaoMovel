777b656659bd5e42de2f28048462adf67b1379e3
jmbm63 <84843794+jmbm63@users.noreply.github.com>
2024-02-14 14:36:42 +0000

    envio de imagens

--- app/src/main/java/pt/g2/Jorge/Adapters/MessageAdapter.kt
+++ app/src/main/java/pt/g2/Jorge/Adapters/MessageAdapter.kt
@@ -1,9 +1,11 @@
 package pt.g2.Jorge.Adapters
 
+import android.net.Uri
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
@@ -12,18 +14,18 @@ import pt.g2.Jorge.R
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
+import com.bumptech.glide.Glide
 
 
 data class ChatMessage(
-    var id:String = "",
+    var id: String = "",
     var chatId: String = "",
     var userId: String = "",
     var date: Long = 0,
     var body: String = "",
-    var messagetype:Int=0, // vamos ter que dar valores para quando fizermos as imagens, 1 texto, 2 imagens seq
-    var readby: List<String> = emptyList()
+    var messagetype: Int = 0, // 1 texto, 2 imagens seq
+    var readby: List<Byte> = emptyList()
 )
-
 
 enum class MessageType(val value: Int) {
     TEXT(1),
@@ -34,6 +36,8 @@ enum class MessageType(val value: Int) {
     AUDIO(6)
 }
 
+
+// recycler view adapter
 class MessageAdapter(private val messages: List<ChatMessage>, private val userId: String) :
     RecyclerView.Adapter<MessageAdapter.MessageViewHolder>() {
 
@@ -45,55 +49,60 @@ class MessageAdapter(private val messages: List<ChatMessage>, private val userId
     inner class MessageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val messageText: TextView = itemView.findViewById(R.id.textView_message_text)
         val timestamp: TextView = itemView.findViewById(R.id.textView_message_time)
+        val imageView: ImageView = itemView.findViewById(R.id.imageView_message_image)
     }
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageViewHolder {
         if (viewType == MESSAGE_TYPE_send) {
-            val view =
-                LayoutInflater.from(parent.context).inflate(R.layout.text_message_send, parent, false)
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.text_message_send, parent, false)
             return MessageViewHolder(view)
         } else {
-            val view =
-                LayoutInflater.from(parent.context).inflate(R.layout.text_message_receiver, parent, false)
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.text_message_receiver, parent, false)
             return MessageViewHolder(view)
         }
-
     }
 
     override fun getItemViewType(position: Int): Int {
         firebaseUser =FirebaseAuth.getInstance().currentUser
+
         if(messages[position].userId == firebaseUser!!.uid){
             return MESSAGE_TYPE_send
         }else{
             return MESSAGE_TYPE_receiver
         }
 
-
-
-        //val message = messages[position]
-
-        // Implement your logic to determine the view type based on the message and userId
-        // For example, you can compare the userId with the sender's id in the message
-        // and return MESSAGE_TYPE_send or MESSAGE_TYPE_receiver accordingly.
-
-        // Replace 'senderId' and 'message.senderId' with your actual user ID comparison logic.
-    //return if (message.userId == userId) {
-    //   MESSAGE_TYPE_send
-    // } else {
-    //    MESSAGE_TYPE_receiver
-    // }
     }
 
     override fun onBindViewHolder(holder: MessageViewHolder, position: Int) {
 
-        val sortedMessages = messages.sortedBy { it.date } // arranjar lista pra ficar em ordem
-
+        val sortedMessages = messages.sortedBy { it.date }
         val message = sortedMessages[position]
 
-        holder.messageText.text = message.body
+        // Display text message if it's not an image message
+        if (message.messagetype == MessageType.TEXT.value) {
+            holder.messageText.text = message.body
+            holder.messageText.visibility = View.VISIBLE
+            holder.imageView.visibility = View.GONE
+
+        } else  if (message.messagetype == MessageType.IMAGE.value) {
+
+            // If it's an image message, load the image using Glide
+            val imageUri = Uri.parse(message.body)
+            Glide.with(holder.itemView.context)
+                .load(imageUri)
+                .fitCenter()
+                .into(holder.imageView)
+
+            // Hide the text view
+            holder.messageText.visibility = View.GONE
+            holder.imageView.visibility = View.VISIBLE
+
+        }
+
         holder.timestamp.text = formatTimestamp(message.date)
     }
+
 
     override fun getItemCount(): Int {
         return messages.size
--- app/src/main/java/pt/g2/Jorge/Chats/ChatActivity.kt
+++ app/src/main/java/pt/g2/Jorge/Chats/ChatActivity.kt
@@ -1,7 +1,11 @@
 package pt.g2.Jorge.Chats
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.widget.EditText
 import android.widget.ImageView
@@ -9,16 +13,20 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.model.GlideUrl
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ListenerRegistration
+import com.google.firebase.storage.FirebaseStorage
 import pt.g2.Jorge.Adapters.ChatMessage
 import pt.g2.Jorge.Adapters.MessageAdapter
+import pt.g2.Jorge.Adapters.MessageType
 
 import pt.g2.Jorge.R
+import java.io.InputStream
 import java.util.Date
-import kotlin.math.roundToInt
 
 
 class ChatActivity : AppCompatActivity() {
@@ -30,12 +38,16 @@ class ChatActivity : AppCompatActivity() {
     private lateinit var messagesListener: ListenerRegistration
     private val messagesList = ArrayList<ChatMessage>()
 
+    private val PICK_IMAGE_REQUEST = 1
+
+    private var selectedImageUri: Uri ?= null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
 
-        val userName =
-            intent.getStringExtra("userName") // retrieve the username from the previous activity
+
+        val userName = intent.getStringExtra("userName") // retrieve the username from the previous activity
         val userMessageId = intent.getStringExtra("userId")
         val messageId = intent.getStringExtra("chatId")
 
@@ -48,6 +60,7 @@ class ChatActivity : AppCompatActivity() {
 
         val sendButton = findViewById<ImageView>(R.id.sendMessage)
         val backspace = findViewById<ImageView>(R.id.backspace)
+        val senImageButton= findViewById<ImageView>(R.id.sendImage)
 
 
         messagesRecyclerView = findViewById(R.id.message)
@@ -59,12 +72,21 @@ class ChatActivity : AppCompatActivity() {
             setupMessagesListener(messageId)
         }
 
+        // send functions text and image
         sendButton.setOnClickListener {
             //Log.d("username", "ehehe")
             if (userMessageId != null && messageId != null) {
                 sendMessage(userMessageId, messageId)
             }
         }
+
+        senImageButton.setOnClickListener {
+            if (userMessageId != null && messageId != null) {
+                openImageFolder(userMessageId, messageId)
+            }
+        }
+
+
 
         if (userName != null) {
             //Log.d("username", "Entrei")
@@ -74,22 +96,50 @@ class ChatActivity : AppCompatActivity() {
         backspace.setOnClickListener {
             finish() // go back to the  chat list activity
         }
+    }
+
+   override fun onStart() {
+        super.onStart()
+        val messageId = intent.getStringExtra("chatId")
+        if (messageId != null) {
+            setRealTimeMessage(messageId)
+        }
+   }
+
+    // talvez possa ser retirado
+    override fun onStop() {
+        super.onStop()
+        removeMessagesListener()
+    }
 
 
+
+    private fun removeMessagesListener() {
+        if (::messagesListener.isInitialized) {
+            messagesListener.remove()
+        }
     }
 
     /**
-     * func to show chats done when a chat is opened
+     * opens image folder on mobile phone
+     */
+    private fun openImageFolder (userMessageId: String, messageId: String){
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(intent, PICK_IMAGE_REQUEST)
+    }
+
+
+    /**
+     * func to load chats done when a chat is opened ordered by date
      */
     private fun setupMessagesListener(messageId: String) {
 
-        val messagesCollection =
-            firestore.collection("chats").document(messageId).collection("messages")
+        val messagesCollection = firestore.collection("chats").document(messageId).collection("messages")
 
         // Get the current message's timestamp
         val currentMessageTimestamp = Date().time
 
-        messagesListener = messagesCollection.orderBy("date").endAt(currentMessageTimestamp)
+        messagesListener = messagesCollection.orderBy("date").endAt(currentMessageTimestamp) // order messages by date
             .addSnapshotListener { snapshot, _ ->
                 if (snapshot != null && !snapshot.isEmpty) {
                     messagesList.clear()
@@ -112,18 +162,52 @@ class ChatActivity : AppCompatActivity() {
     }
 
     /**
+     * func created to load the message as it is sent
+     */
+
+    private fun setRealTimeMessage(messageId: String) {
+
+        val messagesCollection = firestore.collection("chats").document(messageId).collection("messages")
+        val currentMessageTimestamp = Date().time
+        messagesListener = messagesCollection.orderBy("date").startAt(currentMessageTimestamp)
+            .addSnapshotListener { snapshot, _ ->
+                if (snapshot != null && !snapshot.isEmpty) {
+                    for (document in snapshot.documents) {
+                        val chatMessage = document.toObject(ChatMessage::class.java)
+                        if (chatMessage != null) {
+                            if (chatMessage.userId != auth.currentUser?.uid) {
+                                // Only add the message if the sender is not the current user
+                                messagesList.add(chatMessage)
+                            }
+                        }
+                    }
+                    messagesList.sortBy { it.date }
+                    messagesAdapter.notifyDataSetChanged()
+                    messagesRecyclerView.scrollToPosition(messagesList.size - 1)
+                }
+            }
+
+    }
+
+    /**
      * func that stores the messages in firebase
      */
 
-    private fun sendMessage(userMessageId: String, messageId: String) {
+    private fun sendMessage(userMessageId: String, messageId: String){
         val messageBox = findViewById<EditText>(R.id.messageBox)
         val messageText = messageBox.text.toString()
 
-        if (messageText.isNotEmpty()) {
+        if (selectedImageUri != null || messageText.isNotEmpty()) {
             val currentUserId = auth.currentUser?.uid.orEmpty()
 
-            if (currentUserId.isNotEmpty()) {
-                val message = ChatMessage(userMessageId, messageId, currentUserId, Date().time, messageText, 1, emptyList())
+            if (selectedImageUri != null && userMessageId != null && messageId != null) { // para imagens
+                Log.d("MessageAdpater", "Entrei no send message")
+                uploadImageAndSendMessage(userMessageId, messageId, selectedImageUri!!)
+
+            } else if (messageText.isNotEmpty()) { // textos
+
+                // User sent a text message
+                val message = ChatMessage(userMessageId, messageId, currentUserId, Date().time, messageText, MessageType.TEXT.value, emptyList())
 
                 val messagesCollection = firestore.collection("chats").document(messageId).collection("messages")
 
@@ -144,11 +228,15 @@ class ChatActivity : AppCompatActivity() {
                         messagesRecyclerView.scrollToPosition(messagesList.size - 1)
 
                         Log.d("username", "Message sent successfully")
+                        Toast.makeText(applicationContext, "Message sent successfully", Toast.LENGTH_SHORT).show()
                     }
-                    .addOnFailureListener { e ->
-                        // Handle failure
-                        Log.d("username", "Error sending message: $e")
-                        Toast.makeText(applicationContext, "Error sending message", Toast.LENGTH_SHORT).show()
+                    .addOnFailureListener {
+                        Log.d("username", "Error sending message: $it")
+                        Toast.makeText(
+                            applicationContext,
+                            "Error sending message",
+                            Toast.LENGTH_SHORT
+                        ).show()
                     }
             }
         } else {
@@ -156,6 +244,64 @@ class ChatActivity : AppCompatActivity() {
         }
     }
 
+
+    /**
+     * Upload Image to firebase firestore and send the image
+     */
+
+    private fun uploadImageAndSendMessage(userMessageId: String, messageId: String, imageUri: Uri) {
+        val currentUserId = auth.currentUser?.uid.orEmpty()
+        val messagesCollection = firestore.collection("chats").document(messageId).collection("messages")
+
+        // Convert the image URI to a byte array
+        val imageStream = contentResolver.openInputStream(imageUri)
+        val imageBytes = imageStream?.readBytes()
+
+        if (imageBytes != null) {
+            // Create a message object with image data
+            val message = ChatMessage(userMessageId, messageId, currentUserId, Date().time,imageUri.toString(), MessageType.IMAGE.value, emptyList())
+            Log.d("MessageAdapter",imageUri.toString())
+
+
+            // Upload the image to Firebase Storage
+            val storageRef = FirebaseStorage.getInstance().reference.child("messages/${messageId}/${System.currentTimeMillis()}.jpg")
+            val uploadTask = storageRef.putBytes(imageBytes)
+
+            uploadTask.addOnSuccessListener { taskSnapshot ->
+                // Get the download URL of the uploaded image
+                val downloadUrl = taskSnapshot.storage.downloadUrl
+                Log.d("MessageAdapter", downloadUrl.toString())
+                // Add the message to Firestore with the download URL
+                downloadUrl.addOnSuccessListener { url ->
+
+                    messagesCollection.add(message)
+                        .addOnSuccessListener { documentReference ->
+                            // Message has been successfully added to Firestore, update UI
+                            val messageId = documentReference.id
+                            message.id = messageId
+
+                            // Add the new message to the messagesList
+                            messagesList.add(message)
+
+                            // Notify the adapter that a new item has been inserted
+                            messagesAdapter.notifyItemInserted(messagesList.size - 1)
+
+                            // Clear the selectedImageUri after sending the message
+                            selectedImageUri = null
+
+                            messagesRecyclerView.scrollToPosition(messagesList.size - 1)
+
+                            Log.d("username", "Image sent successfully")
+                            Toast.makeText(applicationContext, "Image sent successfully", Toast.LENGTH_SHORT).show()
+                        }
+                        .addOnFailureListener {
+                            Log.d("username", "Failed to send image: ${it.message}")
+                            Toast.makeText(applicationContext, "Failed to send image: ${it.message}", Toast.LENGTH_SHORT).show()
+                        }
+                }
+            }
+        }
+    }
 
 
     /**
@@ -166,7 +312,31 @@ class ChatActivity : AppCompatActivity() {
         val profileName = findViewById<TextView>(R.id.profileName)
         profileName.text=userName
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null && data.data != null) {
+            selectedImageUri  = data.data!!
+
+            Log.d("MessageAdapter", "Selected Image Uri: $selectedImageUri ")
+
+            // Call the sendMessage function with the selected image only if selectedImageUri is not null
+            if (selectedImageUri != null) {
+                val userMessageId = intent.getStringExtra("userId")
+                val messageId = intent.getStringExtra("chatId")
+
+                if (userMessageId != null && messageId != null) {
+                    sendMessage(userMessageId, messageId)
+                }
+            }
+        }
+    }
 }
+
+
+
+
 
 
 
